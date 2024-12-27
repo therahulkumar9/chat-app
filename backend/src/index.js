a148d82190ee92cf74bf3d98a8c3e5bb5c6d7ed7
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-28 05:12:08 +0530

    Add API signup endpoint with database integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello from index.js");
+import express from "express";
+
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectDB();
+});
