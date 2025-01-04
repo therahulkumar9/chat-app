a6caf12fe7ebf7feb43b73afa2ae6dd6652df581
Rahul Kumar <therahulkumar135@gmail.com>
2025-01-04 22:21:37 +0530

    Added message route and message controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 dotenv.config();
 
