7dc995d39e71935876b0a23994ab13311d0370f4
Rahul Kumar <therahulkumar135@gmail.com>
2025-01-06 17:15:18 +0530

    Add ProfilePage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
-import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { Link } from "react-router-dom"
+import { useAuthStore } from "../store/useAuthStore"
+import { LogOut, MessageSquare, Settings, User } from "lucide-react"
 
 const Navbar = () => {
-  const { logout, authUser } = useAuthStore();
+  const { logout, authUser } = useAuthStore()
 
   return (
     <header
@@ -13,7 +13,10 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
@@ -28,6 +31,7 @@ const Navbar = () => {
               btn btn-sm gap-2 transition-colors
               
               `}
+              title="Settings"
             >
               <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
@@ -35,12 +39,16 @@ const Navbar = () => {
 
             {authUser && (
               <>
-                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-                  <User className="size-5" />
+                <Link to={"/profile"}  title="Profile"  className={`btn btn-sm gap-2`}>
+                  <User className="size-5"/>
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 items-center" onClick={logout}>
+                <button
+                  className="flex gap-2 items-center"
+                  title="Click to log out"
+                  onClick={logout}
+                >
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
@@ -50,6 +58,6 @@ const Navbar = () => {
         </div>
       </div>
     </header>
-  );
-};
-export default Navbar;
+  )
+}
+export default Navbar
