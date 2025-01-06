264959b6d2944247ddf0512983c20c89060f0a59
Rahul Kumar <therahulkumar135@gmail.com>
2025-01-07 00:57:51 +0530

    Backend is working now

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,42 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import path from "path";
 
-import { connectDB } from "./lib/db.js"
+import { connectDB } from "./lib/db.js";
 
-const app = express()
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-app.use(express.json())
-app.use(cookieParser())
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
-)
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-dotenv.config()
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = process.env.PORT
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT)
-  connectDB()
-})
+server.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
