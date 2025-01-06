ceae392118e0bbe24b45549e22d86d2968cb3e60
Rahul Kumar <therahulkumar135@gmail.com>
2025-01-06 17:30:47 +0530

    Add favicon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,9 +18,10 @@ const Navbar = () => {
               className="flex items-center gap-2.5 hover:opacity-80 transition-all"
             >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
+                {/* <MessageSquare className="w-5 h-5 text-primary" /> */}
+                <img src="/chat.png" alt="" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">CHAT APP</h1>
             </Link>
           </div>
 
